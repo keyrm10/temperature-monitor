bca1b827d388dd7bbf8764a348a0b0946910b3d6
Trung DN <keyrm10@gmail.com>
2025-04-10 10:35:09 +0100

    feat(terraform): implement encrypted Azure credentials using SOPS for authentication

--- terraform/main.tf
+++ terraform/main.tf
@@ -6,9 +6,25 @@ terraform {
       source  = "hashicorp/azurerm"
       version = "~> 4.26.0"
     }
+    sops = {
+      source  = "carlpett/sops"
+      version = "~> 1.2.0"
+    }
   }
+}
+
+data "sops_file" "azure_secrets" {
+  source_file = "${path.module}/azure.enc.yaml"
+}
+
+locals {
+  azure_secrets = yamldecode(data.sops_file.azure_secrets.raw)
 }
 
 provider "azurerm" {
   features {}
+  subscription_id = local.azure_secrets.AZURE_SUBSCRIPTION_ID
+  client_id       = local.azure_secrets.AZURE_CLIENT_ID
+  client_secret   = local.azure_secrets.AZURE_CLIENT_SECRET
+  tenant_id       = local.azure_secrets.AZURE_TENANT_ID
 }
