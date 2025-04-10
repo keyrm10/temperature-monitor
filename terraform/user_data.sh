250addc24a6be92b0a2b3b20a368fe801cd79fdf
Trung DN <keyrm10@gmail.com>
2025-04-10 17:30:31 +0100

    feat(terraform): add cloud-init script to install Docker

---
+++ terraform/user_data.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+
+# Install Docker
+apt update -y
+apt install -y ca-certificates curl
+install -m 0755 -d /etc/apt/keyrings
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
+chmod a+r /etc/apt/keyrings/docker.asc
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
+  $(. /etc/os-release && echo "${UBUNTU_CODENAME:-$VERSION_CODENAME}") stable" | \
+  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+apt update -y
+apt install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+
+# Clone the repository and start services
+git clone https://github.com/keyrm10/temperature-monitor.git /app
+cd /app
+docker compose up -d
