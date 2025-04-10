3f92a376292f611a8a432bd94300f286a3dfa22c
Trung DN <keyrm10@gmail.com>
2025-04-10 09:43:42 +0100

    feat(terraform): initialize Terraform configuration

---
+++ terraform/main.tf
@@ -0,0 +1,14 @@
+terraform {
+  required_version = ">= 1.11.3"
+
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 4.26.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+}
